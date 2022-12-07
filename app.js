762d463dc9467fa6fa4318f0c02b5a84f3f3e908
Apriyanto <apriyantocspro@gmail.com>
2022-12-07 18:06:54 +0700

    add cors

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 require('dotenv').config();
+let cors = require('cors')
 const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
@@ -8,6 +9,7 @@ const parkingRoutes = require('./routers/parking');
 
 const app = express();
 
+app.use(cors())
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
